db57835662bb87eb6fb6c161b805060e7599d7f4
Muneeb <muneeb.k@piserve.com>
2019-05-02 16:20:16 +0530

    add twitter stream using socket.io

--- start_all_apps.sh
+++ start_all_apps.sh
@@ -15,6 +15,9 @@ cd ../node_slave; nohup node index.js >> app.log 2>&1 &
 #cd to the node_slave2 and run the app
 cd ../node_slave2; nohup node index.js >> app.log 2>&1 &
 
+# start the twitter stream app
+cd ../twitter-stream-socket; nohup node index.js >> app.log 2>&1 &
+
 # run all mongo instances
 mongod --dbpath /data/db --smallfiles --port 27017 --config /etc/mongod.conf &
 mongod --dbpath /data/db2 --smallfiles --port 37017 --config /etc/mongod2.conf &
