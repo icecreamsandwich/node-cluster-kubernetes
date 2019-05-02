8c1aba677d4c45ad9c03dbe5a0561e4ff613cfdb
Muneeb <muneeb.k@piserve.com>
2019-05-02 20:17:58 +0530

    experiment twit library

--- twitter-stream-socket/index.js
+++ twitter-stream-socket/index.js
@@ -27,8 +27,21 @@ var T = new Twit({
     access_token : '73091059-kC7EzODlfSsGOI7LLGSaIAMi8dig3q57ZW6hpCaUg',
     access_token_secret : 'D8dGZwLFGzgLK2YN4Lm2uMlj6FcmXDCSfa3QPBi9qF9xp'
 })
+/* //update a status
+T.post('statuses/update', {status : 'Hello twitter from socket'}, function(err, data, response){
+    console.log('status posted');
+    console.log(data);
+}) 
+//Get follower names of a user
+T.get('followers/ids', {screen_name : 'muneebkt'}, function(err, data, response){
+    console.log(data);
+}) */
+// destroy a tweet
+/* T.post('statuses/destroy/:id', {id : '1123955119169703936'}, function(err, data, response){
+    console.log(data)
+}) */
 
-var stream = T.stream('statuses/filter',{track: watchList});
+var stream = T.stream('statuses/filter',{track: watchList, language: 'en'});
 stream.on('tweet', function(data){
     io.sockets.emit('stream', 
     data.created_at+"\n"+data.text)
